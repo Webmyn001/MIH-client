7d900cdfba9d5375990bab06f037cb7ee70f6bf8
Webmyn001 <125801312+Webmyn001@users.noreply.github.com>
2023-03-04 15:38:24 +0100

    Update Formdetails.js

--- src/MakeitHalaal/Form/Formdetails.js
+++ src/MakeitHalaal/Form/Formdetails.js
@@ -98,7 +98,7 @@ const OnchangeShortnote =(e)=> {
  const OnchangefileID = async (e) => { 
    const files = Array.from(e.target.files)
        
-if (images.length === 1 || images.length=== 0 ) {
+if (images.length === 1 && images.length=== 0 ) {
   setImages("")
    alert("please upload two images School ID card and Jamb Admission Letter")
    }else{
