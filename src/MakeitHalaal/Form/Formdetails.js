e1683712234cdecab3cce46222f897812e9984bb
Webmyn001 <125801312+Webmyn001@users.noreply.github.com>
2023-03-05 07:27:37 +0100

    Update Formdetails.js

--- src/MakeitHalaal/Form/Formdetails.js
+++ src/MakeitHalaal/Form/Formdetails.js
@@ -25,7 +25,7 @@ function Formdetails() {
     axios.post("https://repulsive-threads-foal.cyclic.app//api/lessons/add", {Name,School,BankName,AcctName,AcctNo,Whatsapp,Shortnote,images})
     .then((res)=>
     { 
-     alert(" please wait")
+     
     console.log("saved succesfully")
     navigate("/");
     alert("Thank you , form succesfully submitted , we will get back to you on Whatsapp ")
@@ -186,7 +186,7 @@ console.log(Name,School,BankName,AcctName,AcctNo,Whatsapp,Shortnote,images )
                 
                 <div className="flex items-center justify-center gap-x-5 py-7  pb-0">
                  <div className=''>
-                 {loading ? <h2>please wait...</h2>: <button className="bg-[#1048a9] rounded-md px-3 py-2 ">Submit</button>}
+                 {loading ? <h2>please wait...</h2>: <button className="bg-[#1048a9] rounded-md px-3 py-2 " onClick={Load}>Submit</button>}
                  </div>
                </div>  
 
